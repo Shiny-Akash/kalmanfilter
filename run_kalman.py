d44fc5ddfe173b9ebf462916d9e582b016ae50c0
Akash <akasharul2101@gmail.com>
2020-08-11 00:20:35 +0530

    iterate over the data and do kalman filtering

--- run_kalman.py
+++ run_kalman.py
@@ -14,15 +14,26 @@ kfilter = KalmanFilter(dim_x=1, dim_z=1)
 # initialize parameters
 kfilter.x = np.array([0])  # state vector
 kfilter.P = kfilter.P * 100  # covariance matrix of x
-kfilter.Q = kfilter.Q * 0.01 # environment noise
-kfilter.H = np.array([1]) # sensor model
-kfilter.F = np.array([1]) # state transition matrix
-kfilter.B = np.array([1]) # input matrix
+kfilter.Q = kfilter.Q * 0.01  # environment noise
+kfilter.H = np.array([1])  # sensor model
+kfilter.F = np.array([1])  # state transition matrix
+kfilter.B = np.array([1])  # input matrix
 
 # sensor data
 data_length = 10
 truth = np.arange(1, data_length + 1)
-r1 = 0.01
-r2 = 0.06
+r1 = 0.5
+r2 = 0.9
 z1 = truth + np.random.normal(0, r1, data_length)
 z2 = truth + np.random.normal(0, r2, data_length)
+
+# run the loop
+for i in range(data_length):
+    kfilter.predict(u=1)
+    print(
+        "Predicted State : [{:10.6f}] Measured State : [{:10.6f}] Actual State : [{:10.6f}]".format(
+            kfilter.x[0], z1[i], truth[i]
+        )
+    )
+    kfilter.update(z=z1[i], R=r1)
+    kfilter.update(z=z2[i], R=r2)
