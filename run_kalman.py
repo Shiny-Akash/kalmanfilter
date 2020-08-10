cc142bb2a1c82ab0f9a2c5711374361f185dd285
Akash <akasharul2101@gmail.com>
2020-08-10 23:57:41 +0530

    add values for basic parameters

--- run_kalman.py
+++ run_kalman.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-'''runs the kalman filter on the given system'''
+"""runs the kalman filter on the given system"""
 
 # using KalmanFilter from filterpy module
 from filterpy.kalman import KalmanFilter
+
 # numpy for array manipulations
 import numpy as np
 
@@ -11,5 +12,10 @@ import numpy as np
 kfilter = KalmanFilter(dim_x=1, dim_z=1)
 
 # initialize parameters
-kfilter.x = np.array([0]) # state vector
-kfilter.P = np.array([100]) # covariance matrix
+kfilter.x = np.array([0])  # state vector
+kfilter.P = kfilter.P * 100  # covariance matrix of x
+kfilter.Q = kfilter.Q * 0.01 # environment noise
+kfilter.H = np.array([1]) # sensor model
+kfilter.F = np.array([1]) # state transition matrix
+kfilter.B = np.array([1]) # input matrix
+
